e40d3c18942854b36b26e8d807bd39f6672ed83d
Ruoyu Fu <lingoerer@gmail.com>
2017-05-19 17:26:50 +0800

    Initial Commit

---
+++ MetalColor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MetalColor
+//
+//  Created by Ruoyu Fu on 19/5/2017.
+//  Copyright © 2017 Ruoyu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
